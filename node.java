efaaabc026b4b44cbd2dce498e548f2d1c6b53a8
MSL19 <40368820+MSL19@users.noreply.github.com>
2018-08-24 13:34:02 -0600

    weeeeeee

---
+++ node.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+public class node {
+    ArrayList data;
+    public static void main(String[] args){
+
+    }
+    public void addData(int num){
+        data.add(num);
+    }
+    public void
+}
